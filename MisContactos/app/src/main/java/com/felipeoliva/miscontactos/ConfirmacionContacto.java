6b8c4ad1a7cb15aeda04997f0f00df2de36a1673
folivac <felipe.oliva@grupoinnoxus.com>
2017-12-31 19:40:30 -0600

    Se agregó los xmls de los layouts de las dos pantallas requeridas.

---
+++ MisContactos/app/src/main/java/com/felipeoliva/miscontactos/ConfirmacionContacto.java
@@ -0,0 +1,17 @@
+package com.felipeoliva.miscontactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ConfirmacionContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion_contacto);
+
+
+
+    }
+}
--- MisContactos/app/src/main/java/com/felipeoliva/miscontactos/MainActivity.java
+++ MisContactos/app/src/main/java/com/felipeoliva/miscontactos/MainActivity.java
@@ -2,12 +2,29 @@ package com.felipeoliva.miscontactos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText nombreInput = (EditText) findViewById(R.id.etNombre);
+    public EditText fechaInput = (EditText) findViewById(R.id.etFecha);
+    public EditText telefonoInput = (EditText) findViewById(R.id.etTelefono);
+    public EditText emailInput = (EditText) findViewById(R.id.etEmail);
+    public EditText descripcionInput = (EditText) findViewById(R.id.etDescripcion);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String nombre = nombreInput.getText().toString();
+        String fecha = fechaInput.getText().toString();
+        String telefono = telefonoInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String descripcion = descripcionInput.getText().toString();
+
     }
+
 }
